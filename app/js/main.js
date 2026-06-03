05aecc986806cee7d393e9e2d89905c8be1953d5
h0nova <timalysenko502@gmail.com>
2026-06-03 12:39:32 +0300

    added section call-now

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-  $('.feedback__list').slick({
+  $(".feedback__list").slick({
     slidesToShow: 2,
     slidesToScroll: 1,
     arrows: true,
@@ -10,30 +10,34 @@ $(function () {
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: 1
-        }
-      }
-    ]
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 
-  mixitup('.gallery__list', {
+  mixitup(".gallery__list", {
     selectors: {
-      control: '.gallery__btn'
+      control: ".gallery__btn",
     },
     animation: {
-      duration: 300
-    }
+      duration: 300,
+    },
   });
 
-  $('.process__btn').on('click', function () {
-    var index = $(this).closest('.process__item').index();
+  $(".call-now__btn, .header__btn, .about__btn, .service__btn, .contact-us__btn, .footer__btn").on("click", function () {
+    $(".call-now").toggleClass("call-now--active");
+  });
 
-    $('.process__btn').removeClass('process__btn--active');
-    $('.process__item').removeClass('process__item--active');
-    $(this).addClass('process__btn--active');
-    $(this).closest('.process__item').addClass('process__item--active');
+  $(".process__btn").on("click", function () {
+    var index = $(this).closest(".process__item").index();
 
-    $('.process__text-item').removeClass('process__text-item--active');
-    $('.process__text-item').eq(index).addClass('process__text-item--active');
+    $(".process__btn").removeClass("process__btn--active");
+    $(".process__item").removeClass("process__item--active");
+    $(this).addClass("process__btn--active");
+    $(this).closest(".process__item").addClass("process__item--active");
+
+    $(".process__text-item").removeClass("process__text-item--active");
+    $(".process__text-item").eq(index).addClass("process__text-item--active");
   });
 });
